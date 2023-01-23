6c04d631a22d73079f14d1d34482bb07f9728a4e
Letina-sage <letinaroderick@gmail.com>
2023-01-23 01:49:55 -0800

    doubly_functions.c

--- doubly_functions.c
+++ doubly_functions.c
@@ -2,7 +2,7 @@
 
 /**
  *add_dnodeint_end - add a note at the end of the doubly link list
- *@head: first position of linked list
+ *@head: first position of linsked list
  *@n: data to store
  *Return: a doubly linked list
  */
